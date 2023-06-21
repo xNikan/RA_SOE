aa30bece920e63f0c12c1c38910c154dd3935583
xNikan <npersonalman@gmail.com>
2023-06-20 17:42:41 -0700

    Distinguish between counties and data

--- extract_datasets/extract.py
+++ extract_datasets/extract.py
@@ -1,8 +1,14 @@
 import tabula 
 
-pdf_path = "https://github.com/xNikan/RA_SOE/blob/main/extract_datasets/statistical_detail_report_february_2023.pdf"
+# Using Raw File Content link (copy path) in GitHub
+pdf_path = "extract_datasets/statistical_detail_report_february_2023.pdf"
 
-dfs = tabula.read_pdf(pdf_path, stream=True)
+# Grabs county information using area - [y1, x1, y2, x2]
+county = tabula.read_pdf(pdf_path, area=[40, 80, 100, 200], stream=True, pages = 2)
+data = tabula.read_pdf(pdf_path, area = [110, 10, 1000, 1000], stream=True, pages=2)
 
-print(len(dfs))
-#print(dfs[0])
+print(len(county))
+
+
+print(county[0].columns[0])
+print(data[0])
