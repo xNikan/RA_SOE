4a08bf431ba5d6da766b6f4dbe48dc7a921c670c
xNikan <npersonalman@gmail.com>
2023-06-22 18:11:39 -0700

    Read content, removed summary, added county dict

--- extract_datasets/extract.py
+++ extract_datasets/extract.py
@@ -1,14 +1,26 @@
-import tabula 
+# %%
+import tabula
+from collections import  defaultdict
 
 # Using Raw File Content link (copy path) in GitHub
 pdf_path = "extract_datasets/statistical_detail_report_february_2023.pdf"
-
-# Grabs county information using area - [y1, x1, y2, x2]
-county = tabula.read_pdf(pdf_path, area=[40, 80, 100, 200], stream=True, pages = 2)
-data = tabula.read_pdf(pdf_path, area = [110, 10, 1000, 1000], stream=True, pages=2)
-
-print(len(county))
+# "extract_datasets/statistical_detail_report_february_2023.pdf" if running via Run Python
 
 
-print(county[0].columns[0])
-print(data[0])
+# Reads all pages - keep summary to remove later
+all_pages = tabula.read_pdf(pdf_path, pages="1-4", multiple_tables=True)
+
+tables_by_county = defaultdict(list)
+
+
+for i, table in enumerate(all_pages):
+    if set(['Child By Ages', 'Children']) <= set(table.columns):
+        # Grabbing county name to combine with table in dictionary
+        county = tabula.read_pdf(pdf_path, pages=i+1, area=[40, 80, 100, 200], stream=True)[0].columns[0]
+        
+
+        tables_by_county[county].append(table)
+
+
+print(tables_by_county)
+
